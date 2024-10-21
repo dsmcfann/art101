4fc512f2096a5f6b7f23a1fa5345f09b9ed52e2c
Donovan S. <dmcfann@ucsc.edu>
2024-10-21 16:40:22 -0700

    lab 5
    edited the javascript file for lab 5 to print out information about my car. this includes an image of my car, which was not evident in the original screenshots but i added after taking those.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,16 +1,10 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 Javascript
+// Author: Donovan Sayegh-McFann
+// Last Changed: 10/21/24
 
 // Constants
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
@@ -19,3 +13,27 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// A Simple Javascript Program for Lab 5
+// Author: Donovan Sayegh-McFann
+// Created: 10/21/24
+// License: Public Domain
+// (though frankly I'm not sure why you'd want to use this code anyway)
+
+//Defining the Variables of the DonoMobile
+make = "Honda";
+model = "Fit";
+color = "Yellow";
+year = 2016;
+
+// calculating the age of the DonoMobile in 2024
+age = 2024 - year;
+
+// output that will inevitably appear on the website
+// future Donovan or code excavators, not that it is written as WRITELN with an "L" and not WRITEIN with an I
+// also: br is a tag that creates a line break. just makes things look nicer
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + "<br>")
+document.writeln("Age: " + age + " years<br>")
